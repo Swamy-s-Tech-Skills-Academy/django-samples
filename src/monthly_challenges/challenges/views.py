6abd3d167decbaf92304ebfd6540ebacdae83cf3
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-04-26 20:04:13 +0530

    Changes

--- src/monthly_challenges/challenges/views.py
+++ src/monthly_challenges/challenges/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home_view(request):
+    """
+    View function for the home page that renders the index.html template.
+    """
+    return render(request, 'index.html')
--- src/monthly_challenges/monthly_challenges/settings.py
+++ src/monthly_challenges/monthly_challenges/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'challenges',  # Add the challenges app
 ]
 
 MIDDLEWARE = [
@@ -54,13 +56,16 @@ ROOT_URLCONF = 'monthly_challenges.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),  # Add the templates directory
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.static',  # Add static context processor
             ],
         },
     },
@@ -115,6 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Define the static files directory
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
